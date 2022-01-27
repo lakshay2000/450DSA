2083379fa90e4b10bb90a2b3efb161ecd166b9bd
lakshay2000 <bangalakshay2000@gmail.com>
2022-01-28 04:07:04 +0530

    450  DSA series

---
+++ 44.cpp
@@ -0,0 +1,51 @@
+
+// A Program to prints common element in all
+// rows of matrix
+#include <bits/stdc++.h>
+using namespace std;
+
+// Specify number of rows and columns
+#define M 4
+#define N 5
+
+// prints common element in all rows of matrix
+void printCommonElements(int mat[M][N])
+{
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < N; i++)
+    {
+        mp[mat[0][i]] = 1;
+    }
+    for (int i = 1; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (mp[mat[i][j]] == i)
+            {
+                mp[mat[i][j]] = i + 1;
+
+                if (mp[mat[i][j]] == M)
+                {
+                    cout << mat[i][j] << " " << endl;
+                }
+            }
+        }
+    }
+}
+
+// driver program to test above function
+int main()
+{
+    int mat[M][N] =
+        {
+            {1, 2, 1, 4, 8},
+            {3, 7, 8, 5, 1},
+            {8, 7, 7, 3, 1},
+            {8, 1, 2, 7, 9},
+        };
+
+    printCommonElements(mat);
+
+    return 0;
+}
